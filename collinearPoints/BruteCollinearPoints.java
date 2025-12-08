9b2c8224bb6f110ff0dad6dd2930fe92a1fe202b
Chin-Erdene Gantulga <81845324+Chin-eng@users.noreply.github.com>
2025-12-08 22:58:31 +0800

    collinear

--- collinearPoints/BruteCollinearPoints.java
+++ collinearPoints/BruteCollinearPoints.java
@@ -29,7 +29,15 @@ public class BruteCollinearPoints {
             }
         }
 
-        
+        for (int i = 0; i < points.length-3; i++) {
+            for (int j = i+1; j < points.length-2; j++) {
+                for (int k = j + 1; k < points.length-1; k++) {
+                    for (int l = k + 1; l < points.length; l++) {
+                        
+                    }
+                }
+            }
+        }
     }   
     
     public int numberOfSegments() {
